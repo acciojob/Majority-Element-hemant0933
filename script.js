44580dc833667abc3c5c22521abfab73016eecb3
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-21 10:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let arr=[2,1,2];
+function majorityElement(nums){
+	 let count = 0;
+  let candidate = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+}
+
+document.getElementById('output').innerHTML = majorityElement(nums);
